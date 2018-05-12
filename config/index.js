9bbd1399ba910bde9151eb21f0b6d91ed1d78494
chongtianHong <450707109@qq.com>
2018-05-12 21:08:51 +0800

    eleventh commit

--- config/index.js
+++ config/index.js
@@ -19,6 +19,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
+    // host: '192.168.8.36', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     // 是否自动打开浏览器
     autoOpenBrowser: true,
