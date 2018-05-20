e156caa524130e9c59929df7cd603969929dd954
chongtianHong <450707109@qq.com>
2018-05-20 11:54:35 +0800

    seventeenth commit

--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -5,6 +5,10 @@
 </template>
 
 <script type="text/ecmascript-6">
+const LENGTH = 5;
+const CLS_ON = "on";
+const CLS_HALF = "half";
+const CLS_OFF = "off";
 export default {
     name: 'star',
     props: { // 从外层接收两个参数，size和score。
@@ -17,6 +21,9 @@ export default {
     },
     computed: {
         starType () {
+            return "star-" + this.size;
+        },
+        itemClasses () {
             return "star-" + this.size;
         }
     }
@@ -51,10 +58,44 @@ export default {
         }
     }
     &.star-36{
-        
+        .star-item{
+            width: 15px;
+            height: 15px;
+            margin-right: 16px;
+            background-size: 15px 15px;
+        }
+        &:last-child{
+            margin-right: 0;
+        }
+        &.on{
+            background: url('star36_on@2x.png');
+        }
+        &.half{
+            background: url('star36_half@2x.png');
+        }
+        &.off{
+            background: url('star36_off@2x.png');
+        }
     }
     &.star-24{
-        
+        .star-item{
+            width: 10px;
+            height: 10px;
+            margin-right: 3px;
+            background-size: 10px 10px;
+        }
+        &:last-child{
+            margin-right: 0;
+        }
+        &.on{
+            background: url('star24_on@2x.png');
+        }
+        &.half{
+            background: url('star24_half@2x.png');
+        }
+        &.off{
+            background: url('star24_off@2x.png');
+        }
     }
     
 }
