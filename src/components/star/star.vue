d92b2073b5eeef5f587903f2c6a6de025cde9742
chongtianHong <450707109@qq.com>
2018-05-20 12:03:15 +0800

    seventeenth commit

--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -24,7 +24,20 @@ export default {
             return "star-" + this.size;
         },
         itemClasses () {
-            return "star-" + this.size;
+            let result = [];
+            let score = Math.floor(this.score * 2) / 2;
+            let hasDecimal = score % 1 !== 0;
+            let integer = Math.floor(score);
+            for(let i=0; i < integer;i++){
+                result.push(CLS_ON);
+            }
+            if(hasDecimal){
+                result.push(CLS_HALF);
+            }
+            while(result.length<LENGTH){
+                result.push(CLS_OFF);
+            }
+            return result;
         }
     }
 };
