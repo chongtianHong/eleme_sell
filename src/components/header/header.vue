76ae18e5e49b413ac2b5bdcac47f902c4e2738bf
chongtianHong <450707109@qq.com>
2018-05-23 23:03:44 +0800

    seventeenth commit

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -250,6 +250,21 @@ export default {
           padding: 2px 0;
           text-align: center;
         }
+        .title{
+          display: flex; 
+          width: 80%;
+          margin: 30px auto 24px auto;
+          .line{
+            flex: 1;
+            position: relative;
+            top: -6px;
+            border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+          }
+          .text{
+            padding: 0 12px;
+            font-size: 14px;
+          }
+        }
       }
     }
     .detail-close{ // 固定套路
