9e6fde22c76b3ba3839386471f364a2c13f68da4
chongtianHong <450707109@qq.com>
2018-05-26 20:32:19 +0800

    nighteenth commit

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -31,7 +31,8 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%"/>
     </div>
-    <div v-show="detailShow" class="detail" transition="fade">
+    <transition name="fade">
+      <div v-show="detailShow" class="detail">
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <h1 class="name">{{seller.name}}</h1>
@@ -61,6 +62,7 @@
       </div>
       <div class="detail-close" @click="toggleDetail">×</div>
     </div>
+    </transition>
   </div>
 </template>
 
@@ -246,11 +248,11 @@ export default {
     height: 100%;
     overflow: auto; // 若设置为hidden，则当内容高度超出屏幕高度时不能滚动
     transition: all 0.5s; // 对所有属性渐变
+    background: rgba(7,17,27,0.8);
     &.fade-transition{
       opacity: 1;
-      background: rgba(7,17,27,0.8);
     }
-    &.fade-enter-active, &.fade-leave-active{
+    &.fade-enter, &.fade-leave{
       opacity: 0;
       background: rgba(7,17,27,0);
     }
