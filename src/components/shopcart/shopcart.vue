98992bf1b474916ce465a298ad38d4129ffcc69a
chongtianHong <450707109@qq.com>
2018-06-03 20:40:57 +0800

    seventeenth commit

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -2,7 +2,13 @@
   <div class="shopcart">
       <div class="content">
           <div class="content-left">
-              
+              <div class="logo-wrapper">
+                  <div class="logo">
+                      <span class="icon-shopping_cart"></span>
+                  </div>
+              </div>
+              <div class="price"></div>
+              <div class="desc"></div>
           </div>
           <div class="contentright"></div>
       </div>
@@ -14,5 +20,12 @@ export default {};
 </script>
 
 <style lang="stylus" rel="stylesheet/styles">
-
+.icon-shopping_cart{
+    display: inline-block;
+    color:#80858a;
+    width: 100%;
+    height: 100%;
+    background-image : url("cart.png.png") ;
+    background-size: cover;
+  }
 </style>
