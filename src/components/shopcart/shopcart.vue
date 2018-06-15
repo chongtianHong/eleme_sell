bf25a5bee7862aab246fbd3f0ca05bc0fd891061
chongtianHong <450707109@qq.com>
2018-06-15 23:28:57 +0800

    twenty commit

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -23,26 +23,28 @@
             <div class="inner"></div>
         </div>
       </div> -->
-      <div class="shopcart-list" v-show="listShow">
-        <div class="list-header">
-          <h1 class="title">购物车</h1>
-          <span class="empty">清空</span>
+      <transition name="fold">
+        <div class="shopcart-list" v-show="listShow">
+          <div class="list-header">
+            <h1 class="title">购物车</h1>
+            <span class="empty">清空</span>
+          </div>
+          <div class="list-content">
+            <ul>
+              <li class="food" v-for="(food,index) in selectFoods" :key="index">
+                <!-- li下面分为左右两列 -->
+                <span class="name">{{food.name}}</span>
+                <div class="price">
+                  <span>￥{{food.price*food.count}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="list-content">
-          <ul>
-            <li class="food" v-for="(food,index) in selectFoods" :key="index">
-              <!-- li下面分为左右两列 -->
-              <span class="name">{{food.name}}</span>
-              <div class="price">
-                <span>￥{{food.price*food.count}}</span>
-              </div>
-              <div class="cartcontrol-wrapper">
-                <cartcontrol :food="food"></cartcontrol>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+      </transition>
   </div>
 </template>
 
@@ -144,8 +146,8 @@ export default {
     },
     listShow () {
       if (!this.totalCount) {
-        // eslint-disable-next-line
         // 计算属性内不应该对属性值做变更
+        // eslint-disable-next-line
         this.fold = true;
         return false;
       } else {
@@ -156,7 +158,8 @@ export default {
   },
   methods: {
     toggleList () {
-      if (!this.totalCount) { // 当购物车为0时，点击无效
+      if (!this.totalCount) {
+        // 当购物车为0时，点击无效
         return;
       }
       this.fold = !this.fold;
@@ -167,141 +170,171 @@ export default {
 
 <style lang="stylus" rel="stylesheet/styles">
 .shopcart {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    z-index: 50;
-    width: 100%;
-    height: 48px;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  z-index: 50;
+  width: 100%;
+  height: 48px;
 
-    .content {
-        display: flex;
+  .content {
+    display: flex;
+    background: #141d27;
+    color: rgba(255, 255, 255, 0.4);
+
+    .content-left {
+      flex: 1;
+      font-size: 0;
+
+      .logo-wrapper {
+        display: inline-block;
+        position: relative;
+        top: -10px; // (116-96)/2
+        margin: 0 12px; // (36-12)/2
+        padding: 6px;
+        width: 56px; // (88+24)/2
+        height: 56px;
+        box-sizing: border-box;
+        vertical-align: top;
+        border-radius: 50%;
         background: #141d27;
-        color: rgba(255, 255, 255, 0.4);
 
-        .content-left {
-            flex: 1;
-            font-size: 0;
-
-            .logo-wrapper {
-                display: inline-block;
-                position: relative;
-                top: -10px; // (116-96)/2
-                margin: 0 12px; // (36-12)/2
-                padding: 6px;
-                width: 56px; // (88+24)/2
-                height: 56px;
-                box-sizing: border-box;
-                vertical-align: top;
-                border-radius: 50%;
-                background: #141d27;
-
-                .logo {
-                    width: 100%;
-                    height: 100%;
-                    border-radius: 50%;
-                    background: #2b343c;
-                }
-                .logo-blue {
-                    width: 100%;
-                    height: 100%;
-                    border-radius: 50%;
-                    background: rgb(0,160,220);
-                    overflow: hidden;
-                }
-                .icon-shopping_cart {
-                    display: inline-block;
-                    width: 100%;
-                    height: 100%;
-                    background-image: url('cart.png.png');
-                    background-size: cover;
-                }
-                .highLight {
-                    display: inline-block;
-                    color: #80858a;
-                    width: 83%;
-                    height: 85%;
-                    background-image: url('cat2.png');
-                    background-position: 4px 7px;
-                    background-size: cover;
-                }
-                .num {
-                    position: absolute;
-                    top: 0;
-                    right: 0;
-                    width: 24px;
-                    height: 16px;
-                    line-height: 16px;
-                    text-align: center;
-                    border-radius: 16px;
-                    font-size: 9px;
-                    font-weight: 700;
-                    color: #fff;
-                    background: rgb(240, 20, 20);
-                    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4);
-                }
-            }
-
-            .price {
-                display: inline-block;
-                vertical-align: top;
-                margin-top: 12px;
-                line-height: 24px; // (116-44-24)/2
-                padding-right: 12px;
-                box-sizing: border-box;
-                border-right: 1px solid rgba(255, 255, 255, 0.1);
-                font-size: 16px;
-                font-weight: 700;
-                &.highPrice{
-                    color: #fff;
-                }
-            }
-
-            .desc {
-                display: inline-block;
-                vertical-align: top;
-                margin: 12px 0 0 12px;
-                line-height: 24px;
-                font-size: 10px;
-            }
+        .logo {
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+          background: #2b343c;
         }
 
-        .content-right {
-            width: 105px;
-            .pay {
-                height: 48px;
-                line-height: 48px;
-                text-align: center;
-                font-size: 12px;
-                font-weight: 700;
-                &.not-enough{
-                    background: #2b333b;
-                }
-                &.enough{
-                    background: #00b43c;
-                    color: #fff;
-                }
-            }
+        .logo-blue {
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+          background: rgb(0, 160, 220);
+          overflow: hidden;
         }
+
+        .icon-shopping_cart {
+          display: inline-block;
+          width: 100%;
+          height: 100%;
+          background-image: url('cart.png.png');
+          background-size: cover;
+        }
+
+        .highLight {
+          display: inline-block;
+          color: #80858a;
+          width: 83%;
+          height: 85%;
+          background-image: url('cat2.png');
+          background-position: 4px 7px;
+          background-size: cover;
+        }
+
+        .num {
+          position: absolute;
+          top: 0;
+          right: 0;
+          width: 24px;
+          height: 16px;
+          line-height: 16px;
+          text-align: center;
+          border-radius: 16px;
+          font-size: 9px;
+          font-weight: 700;
+          color: #fff;
+          background: rgb(240, 20, 20);
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4);
+        }
+      }
+
+      .price {
+        display: inline-block;
+        vertical-align: top;
+        margin-top: 12px;
+        line-height: 24px; // (116-44-24)/2
+        padding-right: 12px;
+        box-sizing: border-box;
+        border-right: 1px solid rgba(255, 255, 255, 0.1);
+        font-size: 16px;
+        font-weight: 700;
+
+        &.highPrice {
+          color: #fff;
+        }
+      }
+
+      .desc {
+        display: inline-block;
+        vertical-align: top;
+        margin: 12px 0 0 12px;
+        line-height: 24px;
+        font-size: 10px;
+      }
     }
 
-    .ball-container{
-        .ball{
-            position: fixed;
-            left: 32px;
-            bottom: 22px;
-            z-index: 200;
-            &.drop-transition{
-                transition: all 0.4s;
-                .inner{
-                    width: 16px;
-                    height: 16px;
-                    border-radius: 50%;
-                    background: rgb(0,160,220);
-                    transition: all 0.4s;
-                }
-            }
+    .content-right {
+      width: 105px;
+
+      .pay {
+        height: 48px;
+        line-height: 48px;
+        text-align: center;
+        font-size: 12px;
+        font-weight: 700;
+
+        &.not-enough {
+          background: #2b333b;
         }
+
+        &.enough {
+          background: #00b43c;
+          color: #fff;
+        }
+      }
     }
+  }
+
+  .ball-container {
+    .ball {
+      position: fixed;
+      left: 32px;
+      bottom: 22px;
+      z-index: 200;
+
+      &.drop-transition {
+        transition: all 0.4s;
+
+        .inner {
+          width: 16px;
+          height: 16px;
+          border-radius: 50%;
+          background: rgb(0, 160, 220);
+          transition: all 0.4s;
+        }
+      }
+    }
+  }
+
+  .shopcart-list {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    width: 100%;
+    transition: all 0.5s;
+    transform: translate3d(0, -100%, 0);
+
+    .fold-enter-active, .fold-leave-active {
+      transition: all 0.5s linear;
+      transform: translate3d(0, -100%, 0);
+    }
+
+    .fold-enter, .fold-leave-to {
+      transition: all 0.5s;
+      transform: translate3d(0, 0, 0);
+    }
+  }
 }
 </style>
