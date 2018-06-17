b960f5e27aba1d816c10ba1fa46a73a571464926
chongtianHong <450707109@qq.com>
2018-06-17 20:50:50 +0800

    twenty commit

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -12,7 +12,7 @@
                     <h1 class="title">{{food.name}}</h1>
                     <div class="detail">
                         <span class="sell-count">月售{{food.sellCount}}份</span>
-                        <span class="rating">好评率{{food.rating}}</span>
+                        <span class="rating">好评率{{food.rating}}%</span>
                     </div>
                     <div class="price">
                         <span class="now">￥{{food.price}}</span>
@@ -74,16 +74,54 @@ export default {
             width: 100%;
             height: 100%;
         }
+        .back{
+            position: absolute;
+            top: 10px;
+            left: 0;
+            .icon-arrow_lift{
+                display: block;
+                padding: 10px; // 使得点击区域变大
+                font-size: 20px;
+                color: #fff;
+            }
+        }
     }
-    .back{
-        position: absolute;
-        top: 10px;
-        left: 0;
-        .icon-arrow_lift{
-            display: block;
-            padding: 10px; // 使得点击区域变大
-            font-size: 20px;
-            color: #fff;
+    .content{
+        padding: 18px;
+        .title{
+            line-height: 14px;
+            margin-bottom: 8px;
+            font-size: 14px;
+            font-weight: 700;
+            color: rgb(7,17,27);
+        }
+        .detail{
+            margin-bottom: 18px;
+            line-height: 10px;
+            height: 10px;
+            font-size: 0;
+            .sell-count, .rating{
+                font-size: 10px;
+                color: rgb(147,153,159);
+            }
+            .sell-count{
+                margin-right: 12px;
+            }
+        }
+        .price{
+            font-size: 0;
+            font-weight: 700;
+            line-height: 24px;
+            .now{
+              margin-right: 8px;
+              font-size: 14px;
+              color: rgb(240, 20, 20);
+            }
+            .old{
+              text-decoration: line-through;
+              font-size: 10px;
+              color: rgb(147, 153, 149);
+            }
         }
     }
 }
