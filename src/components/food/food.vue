b07730c8d2c5d934aba16e714efc331a732b8b6d
chongtianHong <450707109@qq.com>
2018-06-17 20:12:42 +0800

    twenty commit

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -8,6 +8,17 @@
                         <div class="icon-arrow_lift" @click="goBack"> &lt; </div>
                     </div>
                 </div>
+                <div class="content">
+                    <h1 class="title">{{food.name}}</h1>
+                    <div class="detail">
+                        <span class="sell-count">月售{{food.sellCount}}份</span>
+                        <span class="rating">好评率{{food.rating}}</span>
+                    </div>
+                    <div class="price">
+                        <span class="now">￥{{food.price}}</span>
+                        <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                    </div>
+                </div>
             </div>
         </div>
     </transition>
