d8f00c4fb33d643b235e1f01aaa7f0391e817364
hongxb <450707109@qq.com>
2018-06-23 17:07:07 +0800

    split component

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -25,7 +25,11 @@
                         <div class="buy" v-show="!food.count || food.count===0" @click.stop.prevent="addFirst">加入购物车</div>
                     </transition>
                 </div>
-                <split></split>
+                <split v-show="food.info"></split>
+                <div class="info" v-show="food.info">
+                    <h1 class="title">商品信息</h1>
+                    <p class="text">{{food.info}}</p>
+                </div>
             </div>
         </div>
     </transition>
@@ -154,29 +158,44 @@ export default {
               color: rgb(147, 153, 149);
             }
         }
+        .cartcontrol-wrapper{
+            position: absolute;
+            right: 24px;
+            bottom: 24px;
+        }
+        .buy{
+            position: absolute;
+            right: 22px;
+            bottom: 22px;
+            z-index: 10;
+            height: 24px;
+            line-height: 24px;
+            padding: 0 12px;
+            box-sizing: border-box;
+            border-radius: 12px;
+            font-size: 10px;
+            color: #fff;
+            background: rgb(0,160,220);
+            transition: all 0.2s linear;
+            opacity: 1;
+            &.fade-enter,&.fade-leave{
+                opacity: 0;
+            }
+        }
     }
-    .cartcontrol-wrapper{
-        position: absolute;
-        right: 24px;
-        bottom: 24px;
-    }
-    .buy{
-        position: absolute;
-        right: 22px;
-        bottom: 22px;
-        z-index: 10;
-        height: 24px;
-        line-height: 24px;
-        padding: 0 12px;
-        box-sizing: border-box;
-        border-radius: 12px;
-        font-size: 10px;
-        color: #fff;
-        background: rgb(0,160,220);
-        transition: all 0.2s linear;
-        opacity: 1;
-        &.fade-enter,&.fade-leave{
-            opacity: 0;
+    .info{
+        padding: 18px;
+        .title{
+            line-height: 14px;
+            margin-bottom: 6px;
+            font-size: 14px;
+            color: rgb(7,17,27);
+        }
+        .text{
+            padding: 0 8px;
+            font-size: 12px;
+            line-height: 24px;
+            color: #93999f;
         }
     }
 }
