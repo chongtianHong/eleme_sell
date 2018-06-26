73cda2ba004a4f7e4b6cc2e01695cea261115dba
hongxb <450707109@qq.com>
2018-06-26 21:34:35 +0800

    ratingselect component

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -67,34 +67,40 @@ export default {
         margin: 0 18px; // 因为下面有一条线，如果设置padding会占满整个屏幕
         border-bottom: 1px solid rgba(7,17,27,0.1);
         font-size: 0;
+        .block{
+            display: inline-block;
+            padding: 8px 12px;
+            margin-right: 8px;
+            border-radius: 1px;
+            color: rgb(77,85,93);
+            font-size: 12px;
+            line-height: 16px;
+            &.active{
+                color: #fff;
+            }
+            .count{
+                font-size: 8px;
+                margin-left: 2px;
+            }
+            &.positive{
+                background: rgba(0,160,220,0.2);
+                &.active{
+                    background: rgb(0,160,220);
+                }
+            }
+            &.negative{
+                background: rgba(77,85,93,0.2);
+                &.active{
+                    background: rgb(77,85,93);
+                }
+            }
+        }
     }
-    .block{
-        display: inline-block;
-        padding: 8px 12px;
-        margin-right: 8px;
-        border-radius: 1px;
-        color: rgb(77,85,93);
-        font-size: 12px;
-        line-height: 16px;
-        &.active{
-            color: #fff;
-        }
-        .count{
-            font-size: 8px;
-            margin-left: 2px;
-        }
-        &.positive{
-            background: rgba(0,160,220,0.2);
-            &.active{
-                background: rgb(0,160,220);
-            }
-        }
-        &.negative{
-            background: rgba(77,85,93,0.2);
-            &.active{
-                background: rgb(77,85,93);
-            }
-        }
+    .switch{
+        padding: 12px 18px;
+        line-height: 24px;
+        border-bottom: 1px solid rgba(7,17,27,0.1);
+        color: rbg(147,153,159);
     }
 }
 </style>
