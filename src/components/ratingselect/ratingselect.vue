5901ef00a5c1ffe68ddba10b4747e70b484fc06d
hongxb <450707109@qq.com>
2018-06-26 22:03:42 +0800

    ratingselect component

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="ratingselect">
         <div class="rating-type">
-            <span class="block positive" :class="{'active':selectType===2}">{{desc.all}}<span class="count">47</span></span>
-            <span class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span class="count">40</span></span>
-            <span class="block negative" :class="{'active':selectType===1}">{{desc.negative}}<span class="count">7</span></span>
+            <span @click="select(2,$event)" class="block positive" :class="{'active':selectType===2}">{{desc.all}}<span class="count">47</span></span>
+            <span @click="select(0,$event)" class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span class="count">40</span></span>
+            <span @click="select(1,$event)" class="block negative" :class="{'active':selectType===1}">{{desc.negative}}<span class="count">7</span></span>
         </div>
         <div class="switch" :class="{'on':onlyContent}">
             <span class="icon-check_circle">√</span>
@@ -44,6 +44,14 @@ export default {
           negative: '不满意'
         };
       }
+    }
+  },
+  methods: {
+    select (type, event) {
+      if (!event._constructed) { // 去掉自带click事件的点击
+        return;
+      }
+      this.selectType = type;
     }
   }
 };
