8186f6815761905bf8b471086f65e9c83a6c983d
hongxb <450707109@qq.com>
2018-07-01 13:55:36 +0800

    rating list

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>I am ratings</div>
+  <div class="ratings">
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left"></div>
+        <div class="overview-right"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -9,5 +16,28 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/styles">
-
+  .ratings{
+    position: absolute;
+    top: 174px; // 留出header和tab的高度
+    bottom: 0;
+    width: 100%;
+    left: 0;
+    overflow: hidden;
+    .ratings-content{
+      .overview{
+        display: flex;
+        padding: 18px 0;
+        .overview-left{
+          flex: 0 0 137px; // 先不考虑iphone5的兼容性
+          width: 137px;
+          border-right: 1px solid rgba(7,17,27,0.1);
+          text-align: center;
+        }
+        .overview-right{
+          flex: 1;
+          padding-left: 24px;
+        }
+      }
+    }
+  }
 </style>
