47a3b71e59dd43e7ed1af1010566ace1f7f99150
hongxb <450707109@qq.com>
2018-07-01 15:29:14 +0800

    ratings_vue

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -7,19 +7,38 @@
           <div class="title">综合评分</div>
           <div class="rank">高于周边商家{{seller.rankRate}}%</div>
         </div>
-        <div class="overview-right"></div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :score="seller.serviceScore" :size="36"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :score="seller.foodScore" :size="36"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}</span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import star from '../star/star';
 export default {
   name: 'ratings',
   props: {
     seller: {
       type: Object
     }
+  },
+  components: {
+    star
   }
 };
 </script>
