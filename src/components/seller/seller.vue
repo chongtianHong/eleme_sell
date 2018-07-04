9c6d694380eefb6bdbfeee8808fbf5ff4a1b6583
hongxb <450707109@qq.com>
2018-07-04 23:02:48 +0800

    seller_vue

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -2,19 +2,50 @@
   <div class="seller">
     <div class="seller-content">
       <div class="overview">
-        <h1 class="title"></h1>
+        <h1 class="title">{{seller.name}}</h1>
+        <div class="desc">
+          <star :score="seller.score" :size="36"></star>
+          <span class="text">({{seller.ratingCount}})</span>
+          <span class="text">{{seller.sellCount}}</span>
+        </div>
+        <ul class="remark">
+          <li class="block">
+            <h2>起送价</h2>
+            <div class="content">
+              <span class="stress">{{seller.minPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>平均配送时间</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryTime}}</span>分钟
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import star from '../star/star';
+import split from '../split/split';
 export default {
   name: 'seller',
   props: {
     seller: {
       type: Object
     }
+  },
+  components: {
+    star,
+    split
   }
 };
 </script>
